51e3e748fcafbd5935353bdbc80c605edc44cc9c
gis-14186840 <zhiwei.wu-3@postgrad.manchester.ac.uk>
2025-10-07 12:20:31 +0100

    added week 1 file

--- week1/week1.2.py
+++ week1/week1.2.py
@@ -1,8 +1,17 @@
 from geopandas import read_file
 
 world = read_file("E:/Manchester/UGIS/data/natural-earth/ne_50m_admin_0_countries.shp")
+graticule = read_file("E:/Manchester/UGIS/data/natural-earth/ne_110m_graticules_15.shp")
+bbox = read_file("E:/Manchester/UGIS/data/natural-earth/ne_110m_wgs84_bounding_box.shp")
 
-print(world.head())
+ea_proj = '+proj=eqearth +lon_0=0 +datum=WGS84 +units=m +no_defs'
+# reproject all three layers to equal earth
+world = world.to_crs(ea_proj)
+graticule = graticule.to_crs(ea_proj)
+bbox = bbox.to_crs(ea_proj)
+
+world['pop_density'] = world['POP_EST'] / (world.area / 1000000)
+
 from matplotlib.pyplot import subplots, savefig
 # create map axis object
 my_fig, my_ax = subplots(1, 1, figsize=(16, 10))
@@ -10,28 +19,14 @@ my_fig, my_ax = subplots(1, 1, figsize=(16, 10))
 # plot the countries onto ax
 world.plot(ax = my_ax)
 
-# save the result
-savefig('E:/Manchester/UGIS/understandinggis/week1/out/1.png')
-print("done!")
-
 # turn off the visible axes on the map
 my_ax.axis('off')
-
-graticule = read_file("E:/Manchester/UGIS/data/natural-earth/ne_110m_graticules_15.shp")
-bbox = read_file("E:/Manchester/UGIS/data/natural-earth/ne_110m_wgs84_bounding_box.shp")
 
 # add bounding box and graticule layers
 bbox.plot(
     ax = my_ax,
     color = 'lightgray',
     linewidth = 0,
-    )
-
-# plot the countries
-world.plot(
-    ax = my_ax,
-    color = 'black',
-    linewidth = 0.5,
     )
 
 # plot the graticule
@@ -41,31 +36,20 @@ graticule.plot(
     linewidth = 0.5,
     )
 
-print(world.columns)
 # plot the countries
 world.plot(								# plot the world dataset
     ax = my_ax,						# specify the axis object to draw it to
-    column = 'POP_EST',		# specify the column used to style the dataset
+    column = 'pop_density',		# specify the column used to style the dataset
     cmap = 'OrRd',				# specify the colour map used to style the dataset based on POP_EST
     scheme = 'quantiles',	# specify how the colour map will be mapped to the values in POP_EST
     linewidth = 0.5,			# specify the line width for the country outlines
     edgecolor = 'gray',		# specify the line colour for the country outlines
-    )
-
-ea_proj = '+proj=eqearth +lon_0=0 +datum=WGS84 +units=m +no_defs'
-
-
-# reproject all three layers to equal earth
-world = world.to_crs(ea_proj)
-graticule = graticule.to_crs(ea_proj)
-bbox = bbox.to_crs(ea_proj)
-world['pop_density'] = world['POP_EST'] / (world.area / 1000000)
-print(world.columns)
-
-my_ax.set(title="Population Density: Equal Earth Coordinate Reference System")
-
-legend = 'True',
-legend_kwds = {
+    legend = 'True',
+    legend_kwds = {
     'loc': 'lower left',
     'title': 'Population Density'
     }
+    )
+
+
+my_ax.set(title="Population Density: Equal Earth Coordinate Reference System")
