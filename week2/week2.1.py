7ff73c642b60d318ae84fc20837a05db216d201d
gis-14186840 <zhiwei.wu-3@postgrad.manchester.ac.uk>
2025-10-07 20:50:30 +0100

    message describing changes

--- week2/week2.1.py
+++ week2/week2.1.py
@@ -1,5 +1,7 @@
 from geopandas import read_file, GeoSeries
 
+from matplotlib.pyplot import subplots, savefig, title
+
 # load the shapefile of countries - this gives a table of 12 columns and 246 rows (one per country)
 world = read_file("E:/Manchester/UGIS/data/natural-earth/ne_10m_admin_0_countries.shp")
 
@@ -21,5 +23,29 @@ usa_geom = usa_col.iloc[0]
 
 print(type(usa_geom))
 
+# Extract Mexico data
+mex = world.loc[(world.ISO_A3 == 'MEX')]
+print(type(mex))
+
+mex_col = mex.geometry
+print(type(mex_col))
+
+mex_geom = mex_col.iloc[0]
+print(type(mex_geom))
+
 # calculate the intersection of the geometry objects
 border = usa_geom.intersection(mex_geom)
+
+# create map axis object
+my_fig, my_ax = subplots(1, 1, figsize=(16, 10))
+
+# remove axes
+my_ax.axis('off')
+
+# plot the border
+GeoSeries(border).plot(
+  ax = my_ax
+	)
+
+# save the image
+savefig('./out/first-border.png')
