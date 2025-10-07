9660acfde6630039254452fadf2077755cb0211c
gis-14186840 <zhiwei.wu-3@postgrad.manchester.ac.uk>
2025-10-07 13:42:09 +0100

    added week 2 file

--- week2/week2.1.py
+++ week2/week2.1.py
@@ -5,3 +5,7 @@ Created on Tue Oct  7 13:37:57 2025
 @author: Chris
 """
 
+rom geopandas import read_file, GeoSeries
+
+# load the shapefile of countries - this gives a table of 12 columns and 246 rows (one per country)
+world = read_file("E:/Manchester/UGIS/data/natural-earth/ne_10m_admin_0_countries.shp")
