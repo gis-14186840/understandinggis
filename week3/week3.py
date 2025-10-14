ea152748d94f8a996dab3da7ade2fb6851448009
gis-14186840 <zhiwei.wu-3@postgrad.manchester.ac.uk>
2025-10-14 12:57:30 +0100

    git

---
+++ week3/week3.py
@@ -0,0 +1,11 @@
+# THIS IMPORTS A SQUARE ROOT FUNCTION, WHICH IS A BIG HINT!!
+from math import sqrt
+
+def distance(x1, y1, x2, y2):
+	"""
+	* Use Pythagoras' theorem to measure the distance. This is acceptable in this case because:
+	*	 - the CRS of the data is a local projection
+	*	 - the distances are short
+	*  - computational efficiency is important (as we are making many measurements)
+	"""
+	return # complete this line to return the distance between (x1,y1) and (x2,y2)
