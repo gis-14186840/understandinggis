2916de8a19507c7e7984b2c66619280a79f32162
gis-14186840 <zhiwei.wu-3@postgrad.manchester.ac.uk>
2025-10-14 13:11:52 +0100

    message describing changes

--- week3/week3.py
+++ week3/week3.py
@@ -2,10 +2,14 @@
 from math import sqrt
 
 def distance(x1, y1, x2, y2):
-	"""
-	* Use Pythagoras' theorem to measure the distance. This is acceptable in this case because:
-	*	 - the CRS of the data is a local projection
-	*	 - the distances are short
-	*  - computational efficiency is important (as we are making many measurements)
-	"""
-	return # complete this line to return the distance between (x1,y1) and (x2,y2)
+    """
+    * Use Pythagoras' theorem to measure the distance. This is acceptable in this case because:
+    *     - the CRS of the data is a local projection
+    *     - the distances are short
+    *  - computational efficiency is important (as we are making many measurements)
+    """
+    return sqrt((x2 - x1)**2 + (y2 - y1)**2)
+# complete this line to return the distance between (x1,y1) and (x2,y2)
+result = distance(345678, 456789, 445678, 556789)
+print(f"{result:.2f}")
+
