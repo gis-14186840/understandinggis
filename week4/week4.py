612f4afd0590d3aaddc49288639d0f12137f9d9f
gis-14186840 <zhiwei.wu-3@postgrad.manchester.ac.uk>
2025-10-21 22:56:37 +0100

    message describing changes

--- week4/week4.py
+++ week4/week4.py
@@ -1,24 +1,55 @@
 from geopandas import read_file
 
+from sys import exit
+
 # open a dataset of all countries in the world
 world = read_file("E:/Manchester/UGIS/data/natural-earth/ne_10m_admin_0_countries.shp")
 
-# Constants
-SIMPLIFICATION_PERC = 98
 # British National Grid definition
 OSGB = "EPSG:27700"
 
 # extract the UK, project, and extract the geometry
-uk = world[(world['ISO_A3'] == 'GBR')].to_crs(OSGB).geometry.iloc[0]	# COMPLETE THIS LINE
+uk = world[(world['ISO_A3'] == 'GBR')].to_crs(OSGB).geometry.iloc[0]    # COMPLETE THIS LINE
 
 # report geometry type
 print(f"geometry type: {uk.geom_type}")
 
 
-from sys import exit
-
 # quit the analysis if we are dealing with any geometry but a MultiPolygon
 if uk.geom_type != 'MultiPolygon':
   print("Geometry is not a MultiPolygon, exiting...")
   exit()
-  
+
+
+# initialise variables to hold the coordinates and area of the largest polygon
+biggest_area = 0
+coord_list = []
+
+# loop through each polygon in the multipolygon and find the biggest (mainland Great Britain)
+for poly in uk.geoms:
+
+    # if it is the biggest so far
+    if  poly.area > biggest_area:   # COMPLETE THIS LINE
+    
+        # store the new value for biggest area
+        biggest_area = poly.area
+        
+     # store the coordinates of the polygon
+        coord_list = list(poly.boundary.coords)    
+        # COMPLETE THIS LINE (look at the variables that you defined before the loop)
+        
+print(f"Largest polygon area: {biggest_area}")
+print(f"Original number of nodes: {len(coord_list)}")
+
+
+# set the percentage of nodes that you want to remove
+SIMPLIFICATION_PERC = 98
+
+# how many nodes do we need?
+n_nodes = int(len(coord_list) / 100.0 * (100 - SIMPLIFICATION_PERC))
+
+# ensure that there are at least 3 nodes (minimum for a polygon)
+if n_nodes < 3:
+	n_nodes = 3 
+
+print(f"Target number of nodes: {n_nodes}")
