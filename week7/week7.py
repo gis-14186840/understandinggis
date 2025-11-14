26c75bcff56d949bf7cbc792f43955c3d0a48817
gis-14186840 <zhiwei.wu-3@postgrad.manchester.ac.uk>
2025-11-14 12:06:09 +0000

    update

--- week7/week7.py
+++ week7/week7.py
@@ -1,5 +1,18 @@
 from rasterio import open as rio_open
 
+from rasterio.transform import rowcol, xy
+
+def coord_2_img(transform, x, y):
+    """ 
+    * Convert from coordinate space to image space using the 
+    * 	Affine transform object from a rasterio dataset
+    *
+    * Note that rowcol() returns floats so they need to be 
+    * 	converted to integers to be used as cell references
+    """
+    r, c = rowcol(transform, x, y)
+    return (int(r), int(c))
+
 # open a raster file into a variable called dem
 with rio_open('E:/Manchester/UGIS/data/helvellyn/Helvellyn-50.tif') as dem:
 
@@ -7,8 +20,18 @@ with rio_open('E:/Manchester/UGIS/data/helvellyn/Helvellyn-50.tif') as dem:
     # once you leave the block, the file automatically closes for you
     
     # test the dataset has been opened successfully
-    print(dem.profile)
+    # print(dem.profile)
     
     # get the single data band from a dem
     band_1 = dem.read(1)
     
+    # define Helvellyn coordinates
+    summit_x = 334170
+    summit_y = 515165
+    
+    # transfor coordinate
+    summit_row, summit_col = coord_2_img(dem.transform, summit_x, summit_y)
+    
+    # print result
+    summit_elevation = band_1[summit_row][summit_col]
+    print(f"\nHelvellyn elevation：{summit_elevation:.0f}m")
