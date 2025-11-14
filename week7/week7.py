5af890f2a09f10c12f3c8e66bd5a153bc5b5afe4
gis-14186840 <zhiwei.wu-3@postgrad.manchester.ac.uk>
2025-11-14 11:47:55 +0000

    update

--- week7/week7.py
+++ week7/week7.py
@@ -1,8 +1,14 @@
 from rasterio import open as rio_open
+
 # open a raster file into a variable called dem
 with rio_open('E:/Manchester/UGIS/data/helvellyn/Helvellyn-50.tif') as dem:
 
 	# everything inside the with block can access dem
-  
     # once you leave the block, the file automatically closes for you
+    
+    # test the dataset has been opened successfully
     print(dem.profile)
+    
+    # get the single data band from a dem
+    band_1 = dem.read(1)
+    
