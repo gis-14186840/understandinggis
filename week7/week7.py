1705c58e4301886b656dd3a8e7d61311ff9c29c0
gis-14186840 <zhiwei.wu-3@postgrad.manchester.ac.uk>
2025-11-14 11:38:48 +0000

    update

--- week7/week7.py
+++ week7/week7.py
@@ -1,8 +1,8 @@
 from rasterio import open as rio_open
 # open a raster file into a variable called dem
-with rio_open('./data/helvellyn/Helvellyn-50.tif') as dem:
+with rio_open('E:/Manchester/UGIS/data/helvellyn/Helvellyn-50.tif') as dem:
 
 	# everything inside the with block can access dem
   
-# once you leave the block, the file automatically closes for you
-print(dem.profile)
+    # once you leave the block, the file automatically closes for you
+    print(dem.profile)
